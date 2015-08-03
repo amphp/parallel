72db6e53aa195cdac5cb060c75db73d3a8fbb0ae
coderstephen <me@stephencoakley.com>
2015-08-03 00:19:51 -0500

    Fix dumb mistakes and add close() method

--- src/Sync/Channel.php
+++ src/Sync/Channel.php
@@ -12,14 +12,19 @@ use Icicle\Socket\Stream\DuplexStream;
  */
 class Channel
 {
-    const MSG_DONE = 1;
-    const MSG_ERROR = 2;
-    const MSG_DATA = 3;
+    const MESSAGE_DONE = 1;
+    const MESSAGE_ERROR = 2;
+    const MESSAGE_DATA = 3;
 
-    private static $nextId = 1;
-
-    private $socketResource;
+    /**
+     * @var DuplexStream An asynchronous socket stream.
+     */
     private $socket;
+
+    /**
+     * @var resource A synchronous socket stream.
+     */
+    private $socketResource;
 
     /**
      * Creates a new channel and returns a pair of connections.
@@ -32,6 +37,7 @@ class Channel
      */
     public static function create()
     {
+        // Create a socket pair.
         if (($sockets = stream_socket_pair(STREAM_PF_UNIX, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP)) === false) {
             throw new ChannelException('Failed to create channel sockets.');
         }
@@ -53,7 +59,7 @@ class Channel
         $serialized = serialize($data);
         $length = strlen($serialized);
 
-        $header = pack('CL', self::MSG_DATA, $length);
+        $header = pack('CL', self::MESSAGE_DATA, $length);
         $message = $header.$serialized;
 
         return $this->getSocket()->write($message);
@@ -64,17 +70,22 @@ class Channel
      *
      * @return PromiseInterface
      */
-    public function recieve()
+    public function receive()
     {
         return Coroutine\create(function () {
-            // Read the message header first.
-            $header = (yield $this->getSocket()->read(5));
-            // Extract the data from the header.
-            extract(unpack('Ctype/Llength', $header));
+            // Read the message header first and extract its contents.
+            $header = unpack('Ctype/Llength', (yield $this->getSocket()->read(5)));
+
+            // If the message type is MESSAGE_DONE, the peer was closed and the channel
+            // is done.
+            if ($header['type'] === self::MESSAGE_DONE) {
+                $this->getSocket()->close();
+                return;
+            }
 
             // Read the serialized data from the socket.
-            if ($type === self::MSG_DATA) {
-                $serialized = (yield $this->socket->read($length));
+            if ($header['type'] === self::MESSAGE_DATA) {
+                $serialized = (yield $this->socket->read($header['length']));
                 $data = unserialize($serialized);
 
                 yield $data;
@@ -85,9 +96,32 @@ class Channel
         });
     }
 
+    /**
+     * Closes the channel.
+     *
+     * This method closes the connection to the peer and sends a message to the
+     * peer notifying that the connection has been closed.
+     *
+     * @return PromiseInterface
+     */
     public function close()
     {
-        $this->getSocket()->close();
+        // Create a message with just a DONE header and zero data.
+        $message = pack('Cx4', self::MESSAGE_DONE);
+
+        return $this->getSocket()->write($message)->then(function () {
+            $this->getSocket()->close();
+        });
+    }
+
+    /**
+     * Checks if the channel is still open.
+     *
+     * @return bool True if the channel is open, otherwise false.
+     */
+    public function isOpen()
+    {
+        return $this->getSocket()->isOpen();
     }
 
     /**
