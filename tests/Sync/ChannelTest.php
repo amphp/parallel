fe54634462fc9a12b3fc792bf5a1d43df2d9cfbf
coderstephen <me@stephencoakley.com>
2015-08-03 00:20:06 -0500

    Basic channel test

---
+++ tests/Sync/ChannelTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Icicle\Tests\Concurrent\Sync;
+
+use Icicle\Concurrent\Sync\Channel;
+use Icicle\Loop;
+
+class ChannelTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        list($a, $b) = Channel::create();
+
+        $this->assertInstanceOf(Channel::class, $a);
+        $this->assertInstanceOf(Channel::class, $b);
+    }
+
+    public function testClose()
+    {
+        list($a, $b) = Channel::create();
+
+        // Close $a. $b should close on next read...
+        $a->close();
+        $b->receive();
+
+        Loop\run();
+
+        $this->assertFalse($a->isOpen());
+        $this->assertFalse($b->isOpen());
+    }
+
+    public function testSendReceive()
+    {
+        list($a, $b) = Channel::create();
+
+        $a->send('hello')->then(function () use ($b) {
+            return $b->receive();
+        })->done(function ($data) {
+            $this->assertEquals('hello', $data);
+        });
+
+        Loop\run();
+    }
+}
