453533e09efcd3066cc482e40eb6492b9ab7966a
Aaron Piotrowski <aaron@trowski.com>
2016-01-23 10:57:10 -0600

    Return if sending result fails
    If sending the actual result failed, sending the reason won't work either. :)

--- src/Forking/Fork.php
+++ src/Forking/Fork.php
@@ -254,8 +254,8 @@ class Fork implements Process, Strand
         try {
             yield $channel->send($result);
         } catch (\Exception $exception) {
-            // The result was not sendable! Try sending the reason why instead.
-            yield $channel->send(new ExitFailure($exception));
+            // The result was not sendable! The parent context must have died or killed the context.
+            yield 0;
         }
     }
 
