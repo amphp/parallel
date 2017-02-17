0912d390049016758538af9c33ed8508b4b7f8d5
Aaron Piotrowski <aaron@trowski.com>
2017-02-17 17:00:24 -0600

    Remove WorkerFork from DefaultWorkerFactory

--- lib/Worker/DefaultWorkerFactory.php
+++ lib/Worker/DefaultWorkerFactory.php
@@ -2,7 +2,7 @@
 
 namespace Amp\Parallel\Worker;
 
-use Amp\Parallel\{ Forking\Fork, Threading\Thread };
+use Amp\Parallel\Threading\Thread;
 
 /**
  * The built-in worker factory type.
@@ -12,16 +12,14 @@ class DefaultWorkerFactory implements WorkerFactory {
      * {@inheritdoc}
      *
      * The type of worker created depends on the extensions available. If multi-threading is enabled, a WorkerThread
-     * will be created. If threads are not available, a WorkerFork will be created if forking is available, otherwise
-     * a WorkerProcess will be created.
+     * will be created. If threads are not available a WorkerProcess will be created.
+     *
+     * \Amp\Parallel\Forking\WorkerFork is not used in the default factory since forking a process for workers is very
+     * sensitive to timing and process state and should be used only by the application designer if desired.
      */
     public function create(): Worker {
         if (Thread::supported()) {
             return new WorkerThread;
-        }
-
-        if (Fork::supported()) {
-            return new WorkerFork;
         }
 
         return new WorkerProcess;
