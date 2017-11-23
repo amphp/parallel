689de9a28b1a484a28a81b92d940748d9e9b21df
Aaron Piotrowski <aaron@trowski.com>
2017-11-22 20:42:56 -0600

    Assert autoload of Task class did not fail
    Fixes #27.

--- lib/Worker/Internal/Job.php
+++ lib/Worker/Internal/Job.php
@@ -22,6 +22,11 @@ class Job {
     }
 
     public function getTask(): Task {
+        // Classes that cannot be autoloaded will be unserialized as an instance of __PHP_Incomplete_Class.
+        \assert(
+            $this->task instanceof Task,
+            \sprintf("Classes implementing %s must be autoloadable by the Composer autoloader", Task::class)
+        );
         return $this->task;
     }
 }
