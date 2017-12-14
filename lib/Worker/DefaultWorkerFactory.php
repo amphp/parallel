b9a8108540fe09354ec3dc786eb985a98119bdfd
Aaron Piotrowski <aaron@trowski.com>
2017-12-13 22:18:12 -0600

    Add ability to specify environment class name when creating factory

--- lib/Worker/DefaultWorkerFactory.php
+++ lib/Worker/DefaultWorkerFactory.php
@@ -8,22 +8,44 @@ use Amp\Parallel\Context\Thread;
  * The built-in worker factory type.
  */
 class DefaultWorkerFactory implements WorkerFactory {
+    /** @var string */
+    private $className;
+
+    /**
+     * @param string $envClassName Name of class implementing \Amp\Parallel\Worker\Environment to instigate in each
+     *     worker. Defaults to \Amp\Parallel\Worker\BasicEnvironment.
+     *
+     * @throws \Error If the given class name does not exist or does not implement \Amp\Parallel\Worker\Environment.
+     */
+    public function __construct(string $envClassName = BasicEnvironment::class) {
+        if (!\class_exists($envClassName)) {
+            throw new \Error(\sprintf("Invalid environment class name '%s'", $envClassName));
+        }
+
+        if (!\is_subclass_of($envClassName, Environment::class)) {
+            throw new \Error(\sprintf(
+                "The class '%s' does not implement '%s'",
+                $envClassName,
+                Environment::class
+            ));
+        }
+
+        $this->className = $envClassName;
+    }
+
     /**
      * {@inheritdoc}
      *
      * The type of worker created depends on the extensions available. If multi-threading is enabled, a WorkerThread
      * will be created. If threads are not available a WorkerProcess will be created.
-     *
-     * \Amp\Parallel\Forking\WorkerFork is not used in the default factory since forking a process for workers is very
-     * sensitive to timing and process state and should be used only by the application designer if desired.
      */
     public function create(): Worker {
         if (Thread::supported()) {
-            return new WorkerThread(BasicEnvironment::class);
+            return new WorkerThread($this->className);
         }
 
         return new WorkerProcess(
-            BasicEnvironment::class,
+            $this->className,
             [],
             \getenv("AMP_PHP_BINARY") ?: (\defined("AMP_PHP_BINARY") ? \AMP_PHP_BINARY : null)
         );
