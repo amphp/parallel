bf08e2fdf40bc4160e5b326dcd4521ab908d8a4c
Aaron Piotrowski <aaron@trowski.com>
2018-10-15 00:15:56 -0500

    Remove output buffering in child process; update require scoping

--- lib/Context/Internal/process-runner.php
+++ lib/Context/Internal/process-runner.php
@@ -12,12 +12,6 @@ if (\function_exists("cli_set_process_title")) {
     @\cli_set_process_title("amp-process");
 }
 
-// Redirect all output written using echo, print, printf, etc. to STDERR.
-\ob_start(function ($data) {
-    \fwrite(\STDERR, $data);
-    return '';
-}, 1, \PHP_OUTPUT_HANDLER_CLEANABLE | \PHP_OUTPUT_HANDLER_FLUSHABLE);
-
 (function () {
     $paths = [
         \dirname(__DIR__, 5) . "/autoload.php",
@@ -75,24 +69,22 @@ Loop::run(function () use ($argc, $argv) {
     }
 
     try {
+        if (!isset($argv[0])) {
+            throw new \Error("No script path given");
+        }
+
+        if (!\is_file($argv[0])) {
+            throw new \Error(\sprintf("No script found at '%s' (be sure to provide the full path to the script)", $argv[0]));
+        }
+
         // Protect current scope by requiring script within another function.
-        $callable = (function () use ($argc, $argv): callable {
-            if (!isset($argv[0])) {
-                throw new \Error("No script path given");
-            }
-
-            if (!\is_file($argv[0])) {
-                throw new \Error(\sprintf("No script found at '%s' (be sure to provide the full path to the script)", $argv[0]));
-            }
-
-            $callable = require $argv[0];
-
-            if (!\is_callable($callable)) {
-                throw new \Error(\sprintf("Script '%s' did not return a callable function", $argv[0]));
-            }
-
-            return $callable;
+        $callable = (function () use ($argc, $argv) { // Using $argc so it is available to the required script.
+            return require $argv[0];
         })();
+
+        if (!\is_callable($callable)) {
+            throw new \Error(\sprintf("Script '%s' did not return a callable function", $argv[0]));
+        }
 
         $result = new Sync\ExitSuccess(yield call($callable, $channel));
     } catch (Sync\ChannelException $exception) {
