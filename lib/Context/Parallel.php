1acd54e84889db9486a49eddb1efc3edee7120bb
Aaron Piotrowski <aaron@trowski.com>
2019-05-01 13:30:57 -0500

    Use closure within parallel runner

--- lib/Context/Parallel.php
+++ lib/Context/Parallel.php
@@ -4,7 +4,10 @@ namespace Amp\Parallel\Context;
 
 use Amp\Loop;
 use Amp\Parallel\Sync\ChannelledSocket;
+use Amp\Parallel\Sync\ExitFailure;
 use Amp\Parallel\Sync\ExitResult;
+use Amp\Parallel\Sync\ExitSuccess;
+use Amp\Parallel\Sync\SerializationException;
 use Amp\Parallel\Sync\SynchronizationError;
 use Amp\Promise;
 use parallel\Runtime;
@@ -196,7 +199,42 @@ final class Parallel implements Context
             }
 
             try {
-                Internal\ParallelRunner::run($channel, $path, $argv);
+                Loop::unreference(Loop::repeat(self::EXIT_CHECK_FREQUENCY, function () {
+                    // Timer to give the chance for the PHP VM to be interrupted by Runtime::kill(), since system calls such as
+                    // select() will not be interrupted.
+                }));
+
+                try {
+                    if (!\is_file($path)) {
+                        throw new \Error(\sprintf("No script found at '%s' (be sure to provide the full path to the script)", $path));
+                    }
+
+                    $argc = \array_unshift($argv, $path);
+
+                    try {
+                        // Protect current scope by requiring script within another function.
+                        $callable = (function () use ($argc, $argv): callable { // Using $argc so it is available to the required script.
+                            return require $argv[0];
+                        })();
+                    } catch (\TypeError $exception) {
+                        throw new \Error(\sprintf("Script '%s' did not return a callable function", $path), 0, $exception);
+                    } catch (\ParseError $exception) {
+                        throw new \Error(\sprintf("Script '%s' contains a parse error", $path), 0, $exception);
+                    }
+
+                    $result = new ExitSuccess(Promise\wait(call($callable, $channel)));
+                } catch (\Throwable $exception) {
+                    $result = new ExitFailure($exception);
+                }
+
+                Promise\wait(call(function () use ($channel, $result) {
+                    try {
+                        yield $channel->send($result);
+                    } catch (SerializationException $exception) {
+                        // Serializing the result failed. Send the reason why.
+                        yield $channel->send(new ExitFailure($exception));
+                    }
+                }));
             } catch (\Throwable $exception) {
                 \trigger_error("Could not send result to parent; be sure to shutdown the child before ending the parent", E_USER_ERROR);
                 return 1;
