76853f0b78350e4b6eca9f0e51c72d1aca3eda49
Daniil Gentili <daniil@daniil.it>
2020-02-10 23:33:17 +0100

    Implement FIFO process hub

--- lib/Context/Internal/ProcessHub.php
+++ lib/Context/Internal/ProcessHub.php
@@ -33,7 +33,12 @@ class ProcessHub
     /** @var string|null */
     private $toUnlink;
 
-    public function __construct()
+    /**
+     * Constructor.
+     *
+     * @param boolean $useFIFO Whether to use FIFOs instead of the more reliable UNIX socket server (CHOSEN AUTOMATICALLY, only for testing purposes)
+     */
+    public function __construct(bool $useFIFO = false)
     {
         $isWindows = \strncasecmp(\PHP_OS, "WIN", 3) === 0;
 
@@ -41,32 +46,73 @@ class ProcessHub
             $this->uri = "tcp://127.0.0.1:0";
         } else {
             $suffix = \bin2hex(\random_bytes(10));
-            $path = \sys_get_temp_dir() . "/amp-parallel-ipc-" . $suffix . ".sock";
-            $this->uri = "unix://" . $path;
+            $path = \sys_get_temp_dir()."/amp-parallel-ipc-".$suffix.".sock";
+            $this->uri = "unix://".$path;
             $this->toUnlink = $path;
         }
 
-        $this->server = \stream_socket_server($this->uri, $errno, $errstr, \STREAM_SERVER_BIND | \STREAM_SERVER_LISTEN);
+        if (!$useFIFO) {
+            $this->server = \stream_socket_server($this->uri, $errno, $errstr, \STREAM_SERVER_BIND | \STREAM_SERVER_LISTEN);
+        }
 
+        $fifo = false;
         if (!$this->server) {
-            throw new \RuntimeException(\sprintf("Could not create IPC server: (Errno: %d) %s", $errno, $errstr));
+            if ($isWindows) {
+                throw new \RuntimeException(\sprintf("Could not create IPC server: (Errno: %d) %s", $errno, $errstr));
+            }
+            if (!\posix_mkfifo($path, 0777)) {
+                throw new \RuntimeException(\sprintf("Could not create the FIFO socket, and could not create IPC server: (Errno: %d) %s", $errno, $errstr));
+            }
+            if (!$this->server = \fopen($path, 'r+')) {
+                throw new \RuntimeException(\sprintf("Could not connect to the FIFO socket, and could not create IPC server: (Errno: %d) %s", $errno, $errstr));
+            }
+            \stream_set_blocking($this->server, false);
+            $fifo = true;
+            $this->uri = $path;
         }
 
         if ($isWindows) {
             $name = \stream_socket_get_name($this->server, false);
             $port = \substr($name, \strrpos($name, ":") + 1);
-            $this->uri = "tcp://127.0.0.1:" . $port;
+            $this->uri = "tcp://127.0.0.1:".$port;
         }
 
         $keys = &$this->keys;
         $acceptor = &$this->acceptor;
-        $this->watcher = Loop::onReadable($this->server, static function (string $watcher, $server) use (&$keys, &$acceptor): \Generator {
-            // Error reporting suppressed since stream_socket_accept() emits E_WARNING on client accept failure.
-            if (!$client = @\stream_socket_accept($server, 0)) {  // Timeout of 0 to be non-blocking.
-                return; // Accepting client failed.
+        $this->watcher = Loop::onReadable($this->server, static function (string $watcher, $server) use (&$keys, &$acceptor, &$fifo): \Generator {
+            if ($fifo) {
+                $length = \ord(\fread($server, 1));
+                if (!$length) {
+                    return; // Could not accept, wrong length read
+                }
+                $prefix = \fread($server, $length);
+                $sockets = [
+                    $prefix."1",
+                    $prefix."2",
+                ];
+                foreach ($sockets as $k => &$socket) {
+                    if (@\filetype($socket) !== 'fifo') {
+                        if ($k) {
+                            \fclose($sockets[0]);
+                        }
+                        return; // Is not a FIFO
+                    }
+                    if (!$socket = \fopen($socket, $k ? 'w' : 'r')) {
+                        if ($k) {
+                            \fclose($sockets[0]);
+                        }
+                        return; // Could not open fifo
+                    }
+                }
+                $channel = new ChannelledSocket(...$sockets);
+            } else {
+                // Error reporting suppressed since stream_socket_accept() emits E_WARNING on client accept failure.
+                if (!$client = @\stream_socket_accept($server, 0)) {  // Timeout of 0 to be non-blocking.
+                    return; // Accepting client failed.
+                }
+                $channel = new ChannelledSocket($client, $client);
             }
 
-            $channel = new ChannelledSocket($client, $client);
 
             try {
                 $received = yield Promise\timeout($channel->receive(), self::KEY_RECEIVE_TIMEOUT);
