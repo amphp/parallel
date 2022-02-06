d546af985aedb19e0ff13dd10365b2983439982b
Niklas Keller <me@kelunik.com>
2022-02-06 18:44:22 +0100

    Remove double semicolon

--- src/Worker/TaskRunner.php
+++ src/Worker/TaskRunner.php
@@ -5,7 +5,7 @@ namespace Amp\Parallel\Worker;
 use Amp\Cache\Cache;
 use Amp\CancelledException;
 use Amp\DeferredCancellation;
-use Amp\Future;;
+use Amp\Future;
 use Amp\Parallel\Worker\Internal\JobChannel;
 use Amp\Pipeline\Queue;
 use Amp\Serialization\SerializationException;
