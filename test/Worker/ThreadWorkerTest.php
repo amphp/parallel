f013263aff3fa427843ea49ec398e7136f0befce
Aaron Piotrowski <aaron@trowski.com>
2023-12-27 12:31:22 -0600

    Thread worker tests

--- test/Worker/ThreadWorkerTest.php
+++ test/Worker/ThreadWorkerTest.php
@@ -5,16 +5,21 @@ namespace Amp\Parallel\Test\Worker;
 use Amp\Cancellation;
 use Amp\Parallel\Context\Context;
 use Amp\Parallel\Context\ContextFactory;
-use Amp\Parallel\Context\ProcessContextFactory;
+use Amp\Parallel\Context\ThreadContext;
+use Amp\Parallel\Context\ThreadContextFactory;
 
-class ProcessPoolTest extends AbstractPoolTest
+class ThreadWorkerTest extends AbstractWorkerTest
 {
     public function createContextFactory(): ContextFactory
     {
+        if (!ThreadContext::isSupported()) {
+            $this->markTestSkipped('ext-parallel required');
+        }
+
         return new class implements ContextFactory {
             public function start(array|string $script, ?Cancellation $cancellation = null): Context
             {
-                return (new ProcessContextFactory())->start($script, cancellation: $cancellation);
+                return (new ThreadContextFactory())->start($script, cancellation: $cancellation);
             }
         };
     }
