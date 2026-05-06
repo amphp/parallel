34abd36adf93991cc66ab11f92563846a750c9f2
Niklas Keller <me@kelunik.com>
2026-05-06 20:48:47 +0200

    Fix NAN to string coercion
    Happens on PHP 8.5: Warning: unexpected NAN value was coerced to string in ... on line 3

---
+++ test/Context/FlattenArgumentTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Amp\Parallel\Test\Context;
+
+use PHPUnit\Framework\TestCase;
+use function Amp\Parallel\Context\flattenArgument;
+use function sqrt;
+
+class FlattenArgumentTest extends TestCase
+{
+    public function testNan()
+    {
+        self::assertSame('NAN', flattenArgument(sqrt(-1)));
+    }
+}
